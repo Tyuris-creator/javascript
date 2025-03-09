2de466616ce2bf8a894d9214079f965379847a3a
Michail Tiurin <tyuris@icloud.com>
2025-03-09 17:52:31 +0300

    set

--- getterSetter.js
+++ getterSetter.js
@@ -38,3 +38,47 @@ class Rectangle{
 // const rectangle = new Rectangle(-10000000000, "string"); doesnt make sense
 // поэтому нужен геттер и сеттер
 
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    set firstName(newFirstName){
+        if(typeof newFirstName === 'string' && newFirstName.length > 0){
+            this._firsName = newFirstName;
+        }
+        else {
+            console.error("first name must be a non empty strings");
+        }
+    }
+    set lastName(newLastName){
+        if(typeof newLastName === 'string' && newLastName.length > 0){
+            this._lastName = newLastName;
+        }
+        else {
+            console.error("first name must be a non empty strings");
+        }
+    }
+
+    set age(newAge){
+        if (typeof newAge === "number" && newAge >= 0){
+            this._age = newAge;
+        }
+        else{
+            console.error("Age must be a non-negative number")
+        }
+    }
+
+    get firsName(){
+        return this._firsName;
+    }
+    get lastName(){
+        return this._lastName
+    }
+    get age(){
+        return this._age
+    }
+
+}
+
